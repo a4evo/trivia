c5fba9006607e31334147478836203c9957cbf44
Anastasiia Shtrakh <>
2020-11-18 23:52:17 +0200

    feat: added env and connection to mongodb

--- nest-trivia/src/app.module.ts
+++ nest-trivia/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGO_URL,
+      entities: [__dirname + '**/**.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
